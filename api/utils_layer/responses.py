11c0f0fbeeba0df82614b0e122857839799a11d3
sudopla <j.pla88@hotmail.com>
2022-01-11 14:27:40 -0500

    fixed issues with mypy

--- api/utils_layer/responses.py
+++ api/utils_layer/responses.py
@@ -5,16 +5,17 @@ API response functions
 import json
 import logging
 from typing import Dict, Any, Union
+from aws_lambda_typing import responses
 from utils_layer.encoder import JSONEncoder
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def success_response(result: Dict[str, Any]) -> str:
+def success_response(result: Dict[str, Any]) -> responses.APIGatewayProxyResponseV2:
     """API Successful Response Object"""
 
-    value = {
+    value: responses.APIGatewayProxyResponseV2 = {
         "statusCode": 200,
         "body": json.dumps(result, cls=JSONEncoder),
     }
@@ -22,10 +23,10 @@ def success_response(result: Dict[str, Any]) -> str:
     return value
 
 
-def fail_response(exc: Union[Exception, str], http_status_code=500) -> str:
+def fail_response(exc: Union[Exception, str], http_status_code=500) -> responses.APIGatewayProxyResponseV2:
     """API Fail Respone Object"""
 
-    value = {
+    value: responses.APIGatewayProxyResponseV2 = {
         "statusCode": http_status_code,
         "body": json.dumps(
             {
